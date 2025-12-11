2e70e39f2637df8da602434784ac82382533212e
kaleivillarprado <kalei.villar@alumnos.udit.es>
2025-12-11 21:53:19 +0100

    chartisoffperoyabasta

--- js/script.js
+++ js/script.js
@@ -39,3 +39,54 @@ window.onload = function () {
         duration: 1500
     });
 };
+
+
+/*gráfico barras*/
+function cargaGraficoBarras() {
+    // Datos para generar el gráfico, hay que definir las etiquetas y
+    // los datasets. Hay que definir un color para cada dataset
+    var datos = {
+        labels: ["2020", "2021", "2022", "2023", "2024"],
+        datasets: [{
+            label: "Pisos turísticos en Madrid",
+            backgroundColor: "#d81e1eff",
+            data: [19597,  17400, 15763, 16970, 22435],
+        }],
+    };
+
+    // Configuración del gráfico. Debe incluir imprescindiblemente el
+    // tipo de gráfico y los datos que hemos definido previamente.
+    // Se pueden añadir opciones para personalizar el gráfico
+    var config = {
+        type: "bar",
+        data: datos,
+        options: {
+            scales: {
+                x: {
+                    border: {
+                        color: "black",
+                        width: 2
+                    },
+                    grid: {
+                        display: false
+                    }
+                },
+                y: {
+                    min: 0,
+                    max: 25000,
+
+                    border: {
+                        color: "black",
+                        width: 2
+                    }
+                }
+            }
+        }
+    };
+    var grafico = $("#barras")[0].getContext("2d");
+    new Chart(grafico, config);
+}
+
+$(document).ready(function () {
+    cargaGraficoBarras();
+});
